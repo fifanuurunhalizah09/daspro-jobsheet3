93506b63b5cc7bd853253cd23f0515a0040c04bc
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-09-12 08:12:23 +0700

    jb3

--- SIAKAD09.java
+++ SIAKAD09.java
@@ -7,7 +7,7 @@ public class SIAKAD09 {
             String nama, nim;
             char kelas;
             byte absen;
-            double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+            double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
 
             System.out.print("Masukkan nama: ");
             nama = sc.nextLine();
@@ -17,16 +17,20 @@ public class SIAKAD09 {
             kelas = sc.nextLine().charAt(0);
             System.out.print("Masukkan nomor absen: ");
             absen = sc.nextByte();
-            System.out.print("Masukkan nilai kuis: ");
+            System.out.print("Masukkan nilai kuis (20%): ");
             nilaiKuis = sc.nextDouble();
-            System.out.print("Masukkan nilai tugas: ");
+            System.out.print("Masukkan nilai tugas (15%): ");
             nilaiTugas = sc.nextDouble();
-            System.out.print("Masukkan nilai ujian: ");
-            nilaiUjian = sc.nextDouble();
+            System.out.print("Masukkan nilai UTS (30%):");
+            nilaiUTS = sc.nextDouble();
+            System.out.print("Masukkan nilai UAS (35%):");
+            nilaiUAS = sc.nextDouble();
 
-            nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
-            System.out.println("Nama: "+ nama + "NIM: " + nim);
-            System.out.println("Kelas " + kelas + "Absen: " + absen);
+            nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUTS + nilaiUAS) / 4;
+            System.out.println("Nama: "+ nama);
+            System.out.println("NIM: " + nim);
+            System.out.println("Kelas " + kelas);
+            System.out.println("Absen: " + absen);
             System.out.println("Nilai Akhir: "+ nilaiAkhir);
 
         
