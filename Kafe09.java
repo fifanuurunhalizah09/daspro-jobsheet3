9801205f122a5f1dafcf57c964941e22f48b6101
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-09-12 08:49:16 +0700

    percobaan

---
+++ Kafe09.java
@@ -0,0 +1,12 @@
+public class Kafe09 {
+    
+    public static void main(String[] args) {
+        boolean keanggotaan:
+        int jmlKopi, jmlTeh, jmlRoti:
+        double hargaKopi = 30000.0, hargaTeh = 20000.0, hargaRoti = 25000.0:
+        float diskon = 10 / 100f;
+
+        System.out.println("Masukkan keanggotaan (true/false): ");
+        keanggotaan
+    }
+}
--- SIAKAD09.java
+++ SIAKAD09.java
@@ -17,16 +17,16 @@ public class SIAKAD09 {
             kelas = sc.nextLine().charAt(0);
             System.out.print("Masukkan nomor absen: ");
             absen = sc.nextByte();
-            System.out.print("Masukkan nilai kuis (20%): ");
+            System.out.print("Masukkan nilai kuis: ");
             nilaiKuis = sc.nextDouble();
-            System.out.print("Masukkan nilai tugas (15%): ");
+            System.out.print("Masukkan nilai tugas: ");
             nilaiTugas = sc.nextDouble();
-            System.out.print("Masukkan nilai UTS (30%):");
+            System.out.print("Masukkan nilai UTS:");
             nilaiUTS = sc.nextDouble();
-            System.out.print("Masukkan nilai UAS (35%):");
+            System.out.print("Masukkan nilai UAS:");
             nilaiUAS = sc.nextDouble();
 
-            nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUTS + nilaiUAS) / 4;
+            nilaiAkhir = (nilaiKuis * 0.2 + nilaiTugas * 0.15 + nilaiUTS * 0.3 + nilaiUAS * 0.35);
             System.out.println("Nama: "+ nama);
             System.out.println("NIM: " + nim);
             System.out.println("Kelas " + kelas);
